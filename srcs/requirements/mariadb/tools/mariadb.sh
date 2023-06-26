174011056b1f7a50c8b1ec7f6f8f27a005a70e36
Youssef El-aouny <yel-aoun@e1r3p1.1337.ma>
2023-06-26 20:18:56 +0100

    inception

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+service mysql start
+    echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
+    echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' ;" >> db1.sql
+    echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
+mysql < db1.sql
+service mysql stop
+mysqld
---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
+rm -rf *
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+chmod +x wp-cli.phar 
+mv wp-cli.phar /usr/local/bin/wp
+wp core download --allow-root
+mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+sed -i -r "s/database_name_here/$DB_NAME/1"   wp-config.php
+sed -i -r "s/username_here/$DB_USER/1"  wp-config.php
+sed -i -r "s/password_here/$DB_PASS/1"    wp-config.php
+sed -i -r "s/localhost/$DB_HOST/1"    wp-config.php 
+wp core install --url=localhost/ --title=inception --admin_user=$DB_USER --admin_password=$DB_PASS --admin_email=$yel-aoun@gmail.com --allow-root
+wp user create $WP_USR user42@gmail.com --role=author --user_pass=$WP_PWD --allow-root
+
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+mkdir /run/php
+#It is the default location for PHP-FPM to store its socket files
+
+/usr/sbin/php-fpm7.3 -F
