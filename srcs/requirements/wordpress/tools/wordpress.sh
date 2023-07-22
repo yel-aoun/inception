f920ab27413a590661c3df36ca54a9195d247616
yel-aoun <87442827+yel-aoun@users.noreply.github.com>
2023-07-22 12:47:23 +0100

    Update wordpress.sh
    pass

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -12,7 +12,7 @@ sed -i -r "s/username_here/$DB_USER/1"  wp-config.php
 sed -i -r "s/password_here/$DB_PASS/1"    wp-config.php
 sed -i -r "s/localhost/$DB_HOST/1"    wp-config.php 
 wp core install --url=localhost/ --title=inception --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
-wp user create $DB_USER yel-aoun@gmail.com --role=author --user_pass=$DB_PWD --allow-root
+wp user create $DB_USER yel-aoun@gmail.com --role=author --user_pass=$DB_PASS --allow-root
 
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
