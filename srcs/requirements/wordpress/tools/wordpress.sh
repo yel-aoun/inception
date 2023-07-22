55cac3ccf06dac7bc7f479489af60d3abfdada5d
yel-aoun <87442827+yel-aoun@users.noreply.github.com>
2023-07-22 14:18:01 +0100

    Update wordpress.sh
    commit

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 mkdir -p /var/www/wordpress
 cd /var/www/wordpress
 rm -rf *
@@ -11,8 +10,8 @@ sed -i -r "s/database_name_here/$DB_NAME/1"   wp-config.php
 sed -i -r "s/username_here/$DB_USER/1"  wp-config.php
 sed -i -r "s/password_here/$DB_PASS/1"    wp-config.php
 sed -i -r "s/localhost/$DB_HOST/1"    wp-config.php 
-wp core install --url=localhost/ --title=inception --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
-wp user create $DB_USER yel-aoun@gmail.com --role=author --user_pass=$DB_PASS --allow-root
+wp core install --url=localhost/ --title=inception --admin_user=$DB_USER --admin_password=$DB_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp user create $WP_USR yel-aoun@gmail.com --role=author --user_pass=$WP_PWD --allow-root
 
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
