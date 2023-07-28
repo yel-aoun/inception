8d3a5725a3f95de0adf15203c81cdfc68a5d36f7
Youssef El-aouny <yel-aoun@e1r10p5.1337.ma>
2023-07-28 15:15:33 +0100

    finish

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
-up:
-	docker-compose -f srcs/docker-compose.yml up --build
-
-down :
-	docker-compose -f srcs/docker-compose.yml down
+all: build
+	@docker-compose -f ./srcs/docker-compose.yml up
+re: fclean
+	@docker-compose -f ./srcs/docker-compose.yml build --no-cache
+build:
+	@docker-compose -f ./srcs/docker-compose.yml build
+down:
+	@docker-compose -f ./srcs/docker-compose.yml down
+fclean: down
+	@docker system prune -a --force
+	rm -rf /home/yel-aoun/data/base_data/* && rm -rf /home/yel-aoun/data/wp_data/*
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -10,9 +10,9 @@ mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 sed -i -r "s/database_name_here/$DB_NAME/1"   wp-config.php
 sed -i -r "s/username_here/$DB_USER/1"  wp-config.php
 sed -i -r "s/password_here/$DB_PASS/1"    wp-config.php
-sed -i -r "s/localhost/$DB_HOST/1"    wp-config.php 
-wp core install --url=localhost/ --title=inception --admin_user=$DB_USER --admin_password=$DB_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
-wp user create $WP_USR yel-aoun@gmail.com --role=author --user_pass=$WP_PWD --allow-root
+sed -i -r "s/localhost/$DB_HOST/1"    wp-config.php
+wp core install --url=$DOMAIN_NAME/ --title=inception --admin_user=$DB_USER --admin_password=$DB_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp user create $USR $USR_MAIL --role=author --user_pass=$USR_PASS --allow-root
 
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
